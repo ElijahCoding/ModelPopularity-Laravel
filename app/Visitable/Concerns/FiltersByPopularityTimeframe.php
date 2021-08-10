f2ec55a53e40dfcf917a95f23d28f0adceb9a1b9
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-10 18:26:11 -0400

    building filters by different timeframes

---
+++ app/Visitable/Concerns/FiltersByPopularityTimeframe.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Visitable\Concerns;
+
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait FiltersByPopularityTimeframe
+{
+    public function scopePopularAllTime(Builder $query)
+    {
+        
+    }
+}
--- tests/Feature/PopularityTest.php
+++ tests/Feature/PopularityTest.php
@@ -14,3 +14,12 @@ it('it gets the total visit count', function () {
 
     expect($series->visit_count_total)->toEqual(1);
 });
+
+it('it gets records by all time popularity', function () {
+    Series::factory()->times(2)->create()->each->visit();
+
+    $series = Series::popularAllTime()->get();
+
+    expect($series->count())->toBe(2);
+    expect($series->first()->visit_count_total)->toBe(1);
+});
