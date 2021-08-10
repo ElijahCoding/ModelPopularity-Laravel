0fe45b51feffc6faf0cdc1184cbd954cfbed06a5
ElijahCoding <cuiyeqing960904@gmail.com>
2021-08-09 20:55:03 -0400

    series total count scope with tests

--- app/Http/Controllers/SeriesIndexController.php
+++ app/Http/Controllers/SeriesIndexController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Series;
 use Illuminate\Http\Request;
 
 class SeriesIndexController extends Controller
 {
     public function __invoke()
     {
-        return view('series.index');
+        return view('series.index', [
+            'popular' => Series::query()->get(),
+        ]);
     }
 }
